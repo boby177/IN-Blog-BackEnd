1c220cab4234686c0b847f4c7e1e073f7201ca4d
boby177 <bomasu37@gmail.com>
2022-11-25 15:44:08 +0700

    added jwt lib and upgraded into mysql2 lib

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from "express";
 import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/posts", postRoutes);
-app.use("/api/posts", authRoutes);
-app.use("/api/posts", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(8800, () => {
   console.log("Connected to port 8800!");
